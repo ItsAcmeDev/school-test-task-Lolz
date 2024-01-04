eda1e6c868330526d6f767c8a7b5d9027685c6c5
AcmeDev <81251068+ItsAcmeDev@users.noreply.github.com>
2024-01-04 21:55:23 +0300

    Забыл удалить :)

--- components/WithoutCategory/StateIsMissing.js
+++
@@ -1,9 +0,0 @@
-const locale = require('../../locale').default, sendMessage = require('../Helpers/SendMessage');
-
-module.exports = async (ctx, bot) => {
-    try {
-        return await sendMessage(ctx, bot, locale.IDoNotUnderstand, []);
-    } catch (e) {
-        return await sendMessage(ctx, bot, locale.errorMessage.text, []);
-    }
-}
